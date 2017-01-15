fc16c029a1b1d49d92e95e8b9a1f0d062bdc64ab
Doug Smith <dougsmith2015@gmail.com>
2017-01-14 19:37:31 -0500

    Upgraded twitter.py script to fish for 1000 tweets, 1000 tweets in data.tsv

--- TwitterScrape/twitter.py
+++ TwitterScrape/twitter.py
@@ -8,17 +8,32 @@ secret_key = "sqX8deTdvVJw8WjcwksmjRq7VrAhh69YW1NRhsbnoNaT2EV9Nw"
 access_token = "718548015779311616-r3AcIskbnvQvKnZX4rOKYrCp9TtgR9e"
 access_secret = "QPZkRr49Ogpovwe3t4rJAc9GFkzmLKTqVCIcWXeFtypGL"
 
+#Clean a string
+def clean(instring):
+    outstring = ""
+    for char in instring:
+        if ( (char!="\n") & (char!="\t") & (char!=",") ):
+            outstring+=char
+        else:
+            outstring += " "
+
+    return outstring
+
+
 class tweet_Freq():
     def __init__(self,text,retweets):
-        self.text = text
+        self.text = clean(text)
         self.frequency = retweets
 
 
-
+def get_timeline(api, IDsince):
+    timeline = api.user_timeline(screen_name=uname, since_id=IDsince, include_rts=True, count =200)
+    return timeline
 
 def main():
         tweets = []
         outstring = ""
+        i = 0
 
         auth = tweepy.OAuthHandler(consumer_key, secret_key)
         auth.set_access_token(access_token, access_secret)
@@ -26,8 +41,13 @@ def main():
 
         user = api.get_user(uname);
 
-        timeline = api.user_timeline(screen_name=uname, include_rts=True, count =4000)
-        for tweet in timeline:
+        #Max of querying 200 tweets at a time
+        timeline = api.user_timeline(screen_name=uname, include_rts=True, count =200)
+        '''
+        #Would enable query of current dataset
+        while(1):
+            index = int(raw_input("Inspect: "))
+            tweet = timeline[index]
             print ("ID:", tweet.id)
             print ("User ID:", tweet.user.id)
             print ("Text:", tweet.text)
@@ -46,18 +66,25 @@ def main():
             print ("Retweet count:", tweet.retweet_count)
             print ("Source:", tweet.source)
             print ("Truncated:", tweet.truncated)
-            elem = tweet_Freq(tweet.text, tweet.retweet_count)
-            tweets.append(elem)
+        '''
+        lastID = 0
+        while i < 1000 :
+
+            for tweet in timeline:
+                i += 1
+                elem = tweet_Freq(tweet.text, tweet.retweet_count)
+                tweets.append(elem)
+                lastID = tweet.id
+            timeline = get_timeline(api, lastID)
 
         file = open('data.tsv','w')
 
         for tweet in tweets:
-            print tweet.text + ' : ' + str(tweet.frequency)
+            i += 1
             try:
                 file.write( tweet.text + "\t"+str(tweet.frequency)+"\n")
             except:
                 print "ERROR in writing"
-            outstring += tweet.text + ',' + str(tweet.frequency) + '</br>'
 
         file.close()
 
