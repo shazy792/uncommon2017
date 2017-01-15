7198bb997d5a1ae39547c00941e594d3fda40b53
Geoffrey Owens <geoffowens@uchicago.edu>
2017-01-14 20:25:10 -0600

    Now we select test and training sets randomly

--- scratch/test2.py
+++ scratch/test2.py
@@ -27,20 +27,25 @@ def test_harness(test_tweets, test_scores, test_model, test_vectorizer):
 
 
 yielded = 0
-l = []
+tweets = []
 scores = []
+test_tweets = []
+test_scores = []
 
+import random
 
 for r in get_tweet(filename):
-    if len(r) >= 2:
-            l.append(r[0])
-            scores.append(int(r[1]))
-
+    if random.random() < .8:
+        tweets.append(r[0])
+        scores.append(int(r[1]))
+    else:
+        test_tweets.append(r[0])
+        test_scores.append(int(r[1]))
+        
 import numpy as np
 print np.mean(scores)
-print len(scores)
-cutoff = int(len(scores) * .7)
-print cutoff
+print len(test_tweets)
+print len(tweets)
 
 from sklearn import linear_model
 
@@ -50,11 +55,8 @@ for i in xrange(1,10):
     mf = i*1000
     bigram_vectorizer = TfidfVectorizer(ngram_range=(1,1), token_pattern=r'\b\w+\b', min_df=1, analyzer='word', max_features=mf, stop_words='english')
     analyzer = bigram_vectorizer.build_analyzer()
-    train_set = l[:cutoff]
-    x_2 = bigram_vectorizer.fit_transform(train_set).toarray()
-    clf = linear_model.LogisticRegression()
-    clf.fit(x_2, scores[:cutoff])
+    x_2 = bigram_vectorizer.fit_transform(tweets).toarray()
+    clf = linear_model.PassiveAggressiveRegressor()
+    clf.fit(x_2, scores)
     print "rmse at maxfeatures = {}".format(mf)
-    print test_harness(l[cutoff:], scores[cutoff:], clf, bigram_vectorizer)
-    
-
+    print test_harness(test_tweets, test_scores, clf, bigram_vectorizer)
